e85d1d51ef6da2b39eacdd33240fbed9e400ea68
Javier <javsolsal@alu.edu.gva.es>
2025-11-30 22:26:20 +0100

    Añado código al proyecto

--- ejercicioTelegrama/Form1.Designer.cs
+++ ejercicioTelegrama/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Número: ";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(131, 57);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 4;
+            this.txtNum.TextChanged += new System.EventHandler(this.txtNum_TextChanged);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(131, 141);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(97, 33);
+            this.btnMostrar.TabIndex = 3;
+            this.btnMostrar.Text = "Mostrar tabla";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(341, 259);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnMostrar;
     }
 }
 
--- ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/Form1.cs
@@ -16,5 +16,45 @@ namespace ejercicioTelegrama
         {
             InitializeComponent();
         }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            int num, i;
+            string resultado = "";
+
+            try
+            {
+                num = int.Parse(txtNum.Text);
+
+                if (num < 1)
+                {
+                    MessageBox.Show("Elige un número que sea mayor a 0");
+                    txtNum.Clear();
+                    return;
+                }
+
+                for (i = 1; i <= 10; i++)
+                {
+                    resultado += $"{num} * {i} = {num * i}\n";
+                }
+                txtNum.Clear();
+                txtNum.Focus();
+                MessageBox.Show($"Tabla del {num}\n{resultado}");
+
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void txtNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
